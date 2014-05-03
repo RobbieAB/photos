7a34d80c6b8bfbb056685866b0b54fcb32cbeb53
Robert Bridge <robert@robbieab.com>
2014-05-03 11:33:35 +0100

    Implemented sha256 function.

--- import.py
+++ import.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3.3
 
+import hashlib
+
 imgdir="/home/robert/Pictures/photos/"
+
+def sha256sum(filename):
+    sha = hashlib.sha256()
+    with open(filename, mode='rb') as f:
+        while True:
+            buf=f.read(4096)
+            if not buf:
+                break
+            sha.update(buf)
+    return sha.hexdigest()
+
+print(sha256sum("/home/photos/CARD01/IMG_3268.JPG"))
+
