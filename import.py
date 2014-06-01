a632fd4152f8f9afddd9e5fd9ee56f52a865a1a9
Robert Bridge <robert@robbieab.com>
2014-06-01 11:45:57 +0100

    Latest code tweak

--- import.py
+++ import.py
@@ -1,8 +1,15 @@
-#!/usr/bin/python3.3
+#!/usr/bin/python3.4
+
+import os
 
 import filehash
 
 imgdir="/home/robert/Pictures/photos/"
 
-print(len(filehash.sha256sum("/home/photos/CARD01/IMG_3268.JPG")))
+srcdir="/home/photos/CARD02/"
 
+imglist=os.listdir(srcdir)
+
+for img in imglist :
+    imgpath=os.path.join(srcdir,img)
+    print(filehash.sha256sum(imgpath), imgpath)
